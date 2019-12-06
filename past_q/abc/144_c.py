2f478060caf4a12ec41bdc7236a7cc0325c27ab6
SILKYMAJOR <silky.major@gmail.com>
2019-12-07 00:03:01 +0900

    Add 144

--- past_q/abc/144_c.py
+++ past_q/abc/144_c.py
@@ -1,10 +1,18 @@
-import math
-N = int(input())
-n = math.floor(N**(1/2))
-r = []
-for i in range(1,n+1):
-    if(N%i == 0):
-        r.append(i + N//i)
+def main():
+    N = int(input())
 
-print(min(r)-2)
+    i = 1
+    ans = 10 ** 18
+
+    while i ** 2 <= N:
+        if N % i == 0:
+            j = N/i
+            ans = min(ans, i+j-2)
+        i += 1
+
+    print(int(ans))
+
+
+if __name__ == '__main__':
+    main()
 
